85846c3c67015237abd356395d1e0b9c1001c251
isnaenti <isnaenti16@gmail.com>
2023-12-04 15:01:53 +0700

    upload update menu request

---
+++ chapter 4/Backend/UpdateMenuRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Traits\HasApiResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class UpdateMenuRequest extends FormRequest
+{
+    use HasApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'desc' => 'required|string',
+            'price'=> 'required|numeric',
+            'image'=> 'nullable|image|mimes:png,jpg,jpeg|max:2048',
+            'type'=> 'required|string'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        return $this->responseValidation($validator->errors());
+    }
+}
